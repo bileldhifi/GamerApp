34a21a0902819a8c747d907e80a6d33ae4d3806f
bileldhifi <bileldhifi37@gmail.com>
2023-09-29 21:36:51 +0100

    first commit

---
+++ app/src/main/java/com/example/gamer/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.gamer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        // LOGO splashScreen style
+
+
+    }
+}
