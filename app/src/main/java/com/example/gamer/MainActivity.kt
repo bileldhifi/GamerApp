8126e88803c843b58a5f8add17b253376d4326dd
bileldhifi <bileldhifi37@gmail.com>
2023-10-04 23:39:55 +0100

    update v1.1

--- app/src/main/java/com/example/gamer/MainActivity.kt
+++ app/src/main/java/com/example/gamer/MainActivity.kt
@@ -4,12 +4,19 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        runBlocking {
+            installSplashScreen()
+            delay(1000)
+        }
        setContentView(R.layout.login)
         val btn = findViewById<View>(R.id.buttongoogle)
         val btn1 = findViewById<View>(R.id.buttonfacebook)
