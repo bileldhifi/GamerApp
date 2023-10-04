3920c59a1067f0b274f2800ecd101100359942ac
bileldhifi <bileldhifi37@gmail.com>
2023-10-04 23:12:56 +0100

    update

--- app/src/main/java/com/example/gamer/MainActivity.kt
+++ app/src/main/java/com/example/gamer/MainActivity.kt
@@ -1,14 +1,47 @@
 package com.example.gamer
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+       setContentView(R.layout.login)
+        val btn = findViewById<View>(R.id.buttongoogle)
+        val btn1 = findViewById<View>(R.id.buttonfacebook)
+        //val btn2 = findViewById<View>(R.id.policy)
 
-        // LOGO splashScreen style
+
+        btn.setOnClickListener {
+            val snack = Snackbar.make(it,"Comming soon :)",Snackbar.LENGTH_LONG)
+            snack.show()
+        }
+        btn1.setOnClickListener {
+            val snack = Snackbar.make(it,"Comming soon :)",Snackbar.LENGTH_LONG)
+            snack.show()
+        }
+
+}
+}
+
+
+class register : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registre)
+
+        val btn2 = findViewById<View>(R.id.policy)
+
+
+        btn2.setOnClickListener {
+            val snack = Snackbar.make(it,"Comming soon :)",Snackbar.LENGTH_LONG)
+            snack.show()
+        }
 
 
     }
