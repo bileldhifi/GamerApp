5aeda441b8ddfa23c235887acd9c79b7d305bda1
bileldhifi <bileldhifi37@gmail.com>
2023-10-14 17:34:28 +0100

    update v1.1

--- app/src/main/java/com/example/gamer/MainActivity.kt
+++ app/src/main/java/com/example/gamer/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.gamer
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.delay
@@ -15,12 +18,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         runBlocking {
             installSplashScreen()
-            delay(1000)
+            delay(2000)
         }
-       setContentView(R.layout.login)
+       setContentView(R.layout.activity_loginpage)
+
         val btn = findViewById<View>(R.id.buttongoogle)
         val btn1 = findViewById<View>(R.id.buttonfacebook)
-        //val btn2 = findViewById<View>(R.id.policy)
 
 
         btn.setOnClickListener {
@@ -32,24 +35,36 @@ class MainActivity : AppCompatActivity() {
             snack.show()
         }
 
-}
-}
+        val btnLogin = findViewById<View>(R.id.btnForgetPass)
+        btnLogin.setOnClickListener() {
+            val intent = Intent(this, forgetPassPage::class.java)
+            startActivity(intent)
+            showToast("Button clicked !")
+        }
 
+        val btnregistre = findViewById<View>(R.id.btnregistre)
+        btnregistre.setOnClickListener() {
+            val intent = Intent(this, registrePage::class.java)
+            startActivity(intent)
+            showToast("Button clicked !")
+        }
 
-class register : AppCompatActivity() {
+      /*  val btnSubmit = findViewById<View>(R.id.btnSubmit)
+        btnSubmit.setOnClickListener() {
+            val intent = Intent(this, loginPage::class.java)
+            startActivity(intent)
+        }*/
+
+}
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+class Register : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.registre)
-
-        val btn2 = findViewById<View>(R.id.policy)
-
-
-        btn2.setOnClickListener {
-            val snack = Snackbar.make(it,"Comming soon :)",Snackbar.LENGTH_LONG)
-            snack.show()
-        }
-
+        setContentView(R.layout.activity_registre_page)
 
     }
-}
+}}
